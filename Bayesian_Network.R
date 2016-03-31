b67b58ddddbf6189484c36c54138eefa67c55580
dgod1028 <dgod1028@gmail.com>
2016-04-01 04:00:07 +0900

    Update Bayesian_Network.R

--- Bayesian_Network.R
+++ Bayesian_Network.R
@@ -5,9 +5,9 @@ library(Ecdat)
 data(Fair)
 head(Fair)
 
-data = Fair[,c(-3,-9,-7)]
+data = Fair[,c(-2,-3,-9,-7)]
 data[] = lapply(data,as.factor)
-colnames(data) = c("性別","年齢","子供","宗教","教育","幸福")
+colnames(data) = c("性別","子供","宗教","教育","幸福")
 
 library(deal)
 network.prior<- network(data)
@@ -16,9 +16,10 @@ update <- learn(network.prior, data, prior.distribution)
 network.post <- autosearch(getnetwork(update), data,prior.distribution,trace=FALSE)
 plot(getnetwork(network.post),main="Bayesian Network",showban=TRUE)
 
-#### Ban Listを入れる
-ban <- rbind(cbind(1:6,1),cbind(1:6,2),cbind(1:6,3),cbind(6,1:6))
+ban <- rbind(cbind(1:5,1),cbind(1:5,2),cbind(5,1:5))
 banlist(network.prior) = ban
 update <- learn(network.prior, data, prior.distribution)
 network.post <- autosearch(getnetwork(update), data,prior.distribution,trace=FALSE)
 plot(getnetwork(network.post),main="Bayesian Network",showban=FALSE)
+
+
