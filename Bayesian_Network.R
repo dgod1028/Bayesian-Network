954b50c90ceaad8829c24b6663eb886314079ca5
dgod1028 <dgod1028@gmail.com>
2016-04-08 12:59:13 +0900

    Update Bayesian_Network.R

--- Bayesian_Network.R
+++ Bayesian_Network.R
@@ -40,38 +40,38 @@ prob = localprob(getnetwork(network.post))
 
 
 Pro = function(localprob=prob,A=1:2,B=1:2,C=1:5,D=1:7,E=5){
-PR=0
-for(e in E){
-  for(a in A) { 					 		## a = sex
-	if(a == 1){sex = "male"} else{sex = "female"}
-
-	for(b in B) {
-		if(b ==1){kid = "yes"}else{kid="no"}	## b = kid
-		for(c in C) {   				## c = syukyo
-			rel = as.character(c)
-			for(d in D){				## d = education
-				if(d==1){edu="9"}
-				if(d==2){edu="12"}
-				if(d==3){edu="14"}
-				if(d==4){edu="16"}
-				if(d==5){edu=="17"}
-				if(d==6){edu=="18"}
-				if(d==7){edu=="20"}
-				Temp = 
-				  prob$幸福[e,edu,kid,sex] *
-				  prob$教育[edu,rel,kid,sex] *
-				  prob$宗教[rel,kid,sex] *
-				  prob$子供[kid] *
-				  prob$性別[sex] 
-				PR = PR + as.vector(Temp)
-				
-				}
-			}
-		}
-
-	}
-}
-	return(PR)
+  PR=0
+  for(e in E){
+    for(a in A) { 					 		## a = sex
+      if(a == 1){sex = "male"} else{sex = "female"}
+      
+      for(b in B) {
+        if(b ==1){kid = "yes"}else{kid="no"}	## b = kid
+        for(c in C) {   				## c = syukyo
+          rel = as.character(c)
+          for(d in D){				## d = education
+            if(d==1){edu="9"}
+            if(d==2){edu="12"}
+            if(d==3){edu="14"}
+            if(d==4){edu="16"}
+            if(d==5){edu="17"}
+            if(d==6){edu="18"}
+            if(d==7){edu="20"}
+            Temp = 
+              prob$幸福[e,edu,kid,sex] *
+              prob$教育[edu,rel,kid,sex] *
+              prob$宗教[rel,kid,sex] *
+              prob$子供[kid] *
+              prob$性別[sex] 
+            PR = PR + as.vector(Temp)
+            
+          }
+        }
+      }
+      
+    }
+  }
+  return(PR)
 }
 
 Pr = Pro()				###	P(幸福=5)
@@ -87,7 +87,32 @@ P_NoKid = Pro(B=2)		###	P(幸福 = 5 | 子供 = no )	 * P(子供 = no)
 P_K = P_Kid / Pr			###	P(子供 = yes | 幸福 = 5)	
 P_NK = P_NoKid / Pr		###	P(子供	= no  | 幸福 = 5)
 
-P_M
-P_W
-P_K
-P_NK
+
+
+
+Pr = Pro()				    ###	P(幸福=5)
+P_MAN = Pro(A=1)			###	P(幸福=5 |　性別 = 男) *P(性別 = 男)
+P_Woman = Pro(A=2)		###	P(幸福=5 |　性別 = 女) *P(性別 = 女）
+
+P_M = P_MAN / Pr			###	P(性別 = 男 | 幸福 = 5)
+P_W = P_Woman / Pr		###	P(性別 = 女 | 幸福 = 5)
+
+P_Kid = Pro(B=1)			###	P(幸福 = 5 | 子供 = yes ) * P(子供 = yes)
+P_NoKid = Pro(B=2)		###	P(幸福 = 5 | 子供 = no )	 * P(子供 = no)
+
+P_K = P_Kid / Pr			###	P(子供 = yes | 幸福 = 5)	
+P_NK = P_NoKid / Pr		###	P(子供	= no  | 幸福 = 5)
+
+P_E9 = Pro(D=1)
+P_E9_14 = Pro(D = 1:3) /Pr
+P_E17_20 = Pro(D = 5:7) /Pr
+
+
+cat(paste("P(幸福=5) = ",round(Pr,digits=3),"\n",
+          "P(性別 = 男 | 幸福 = 5) = ", round(P_M,digits = 3),"\n",
+          "P(性別 = 男 | 幸福 = 5) = ", round(P_W,digits = 3),"\n",
+          "P(子供 = yes | 幸福 = 5) = ", round(P_K,digits = 3),"\n",    
+          "P(子供 = no | 幸福 = 5) = ", round(P_NK,digits = 3),"\n",
+          "P(教育 = 9~14 | 幸福 = 5) = ", round(P_E9_14,digits = 3),"\n",
+          "P(教育 = 17~20 | 幸福 = 5) = ", round(P_E17_20,digits = 3),"\n",
+        sep="") )
